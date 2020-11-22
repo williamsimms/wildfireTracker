89ef5b48b6fbcea7a6e2ac33a3a5d36522720adf
williamsimms <williamsimms193@gmail.com>
2020-11-21 19:19:54 -0500

    fixes

---
+++ src/components/LocationInfoBox.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LocationInfoBox = () => {
+  return <div></div>
+}
+
+export default LocationInfoBox
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import { API_KEY } from '../utility/API'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 import './Map.scss'
 
 const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null)
+
+  const markers = eventData.map((ev) => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <LocationMarker
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+        />
+      )
+    }
+    return null
+  })
   return (
     <div className='map'>
       <GoogleMapReact bootstrapURLKeys={{ key: API_KEY }} defaultCenter={center} defaultZoom={zoom}>
-        <LocationMarker lat={center.lat} lng={center.lng} />
+        {markers}
       </GoogleMapReact>
     </div>
   )
